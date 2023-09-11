25e0c01a2d57ec139ea2319b152d9d6811abd293
MOUHCINEHANAFY <hanafymouhcine@gmail.com>
2023-09-11 12:53:59 -0700

    task 4

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if idx < 0 or idx > len(my_list) - 1:
+        return my_list
+    else:
+        fct_list = []
+        for i in my_list:
+                fct_list.append(i)
+        fct_list[idx] = element
+        return (fct_list)
