6d28ad1d2bab68be3157349b375d47e39b7c5915
MOUHCINEHANAFY <hanafymouhcine@gmail.com>
2023-09-11 14:08:03 -0700

    task 100

---
+++ 100-print_python_list_info.c
@@ -0,0 +1,24 @@
+#include <Python.h>
+
+/**
+ * print_python_list_info - This function print basic py info
+ * @p: ThePyObject list.
+ */
+void print_python_list_info(PyObject *p)
+{
+	int lenght;
+    int container, idx;
+	PyObject *obj;
+
+	lenght = Py_SIZE(p);
+	container = ((PyListObject *)p)->allocated;
+	printf("[*] Size of the Python List = %d\n", size);
+	printf("[*] Allocated = %d\n", container);
+
+	for (idx = 0; idx < lenght; idx++)
+	{
+		printf("Element %d: ", idx);
+		obj = PyList_GetItem(p, idx);
+		printf("%s\n", Py_TYPE(obj)->tp_name);
+	}
+}
