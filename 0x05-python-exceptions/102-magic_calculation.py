a1dfa336cd50612f9b22771c2d770f7626523dc1
MOUHCINEHANAFY <hanafymouhcine@gmail.com>
2023-09-25 14:34:53 -0700

    102

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    temp = 0
+    for x in range(1, 3):
+        try:
+            if x > a:
+                raise Exception('Too far')
+
+            result += a ** b / x
+        except:
+            temp = b + a
+            break
+    return temp
