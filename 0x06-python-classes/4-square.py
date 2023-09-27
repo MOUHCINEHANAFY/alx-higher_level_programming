9ddec45891470567117677d79508146aae432b9d
MOUHCINEHANAFY <hanafymouhcine@gmail.com>
2023-09-27 12:34:47 -0700

    4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,56 +1,31 @@
 #!/usr/bin/python3
+"""Définir une classe Square (Carré)."""
+
 
 class Square:
+    """Représente un carré."""
+
     def __init__(self, size=0):
-        """
-        Initialize a Square object with a given size.
+        """Initialiser un nouveau carré.
 
         Args:
-            size (int): The size of the square. Defaults to 0 if not provided.
-
-        Raises:
-            TypeError: If the provided size is not an integer.
-            ValueError: If the provided size is less than 0.
+            size (int): La taille du nouveau carré.
         """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
-
-    def area(self):
-        """
-        Calculate and return the area of the square.
-
-        Returns:
-            int: The area of the square (size squared).
-        """
-        return self.__size ** 2
+        self.size = size
 
     @property
     def size(self):
-        """
-        Get the size of the square.
-
-        Returns:
-            int: The size of the square.
-        """
-        return self.__size
+        """Obtenir/définir la taille actuelle du carré."""
+        return (self.__size)
 
     @size.setter
     def size(self, value):
-        """
-        Set the size of the square.
-
-        Args:
-            value (int): The new size for the square.
-
-        Raises:
-            TypeError: If the provided value is not an integer.
-            ValueError: If the provided value is less than 0.
-        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
+    def area(self):
+        """Retourner la superficie actuelle du carré."""
+        return (self.__size * self.__size)
