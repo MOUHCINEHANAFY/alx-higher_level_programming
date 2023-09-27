135a998ecbdb790264b8faae3a46bd67c6f0c734
MOUHCINEHANAFY <hanafymouhcine@gmail.com>
2023-09-27 12:22:56 -0700

    1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Square class definition"""
+
+
+class Square:
+    """class of square."""
+
+    def __init__(self, size):
+        """Init a Square objct.
+
+        Args:
+            size (int): The size of square objct.
+        """
+        self.__size = size
