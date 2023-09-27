ee1ade4a9e9f03d1af7bf6c8f16cf117bddc6a26
MOUHCINEHANAFY <hanafymouhcine@gmail.com>
2023-09-27 12:25:29 -0700

    2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Square class def."""
+
+
+class Square:
+    """square class."""
+
+    def __init__(self, size=0):
+        """Init Square.
+
+        Args:
+            size (int): The size of square obj.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >=0")
+        self.__size = size
