f2f50a132b9fea97d35bc220491daea72a69567f
MOUHCINEHANAFY <hanafymouhcine@gmail.com>
2023-10-06 04:47:36 -0700

    102

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -6,7 +6,7 @@
  */
 void print_python_string(PyObject *p)
 {
-	long int len;
+	long int lenght;
 
 	fflush(stdout);
 
@@ -17,12 +17,11 @@ void print_python_string(PyObject *p)
 		return;
 	}
 
-	len = ((PyASCIIObject *)(p))->len;
-
+	lenght = ((PyASCIIObject *)(p))->lenght;
 	if (PyUnicode_IS_COMPACT_ASCII(p))
 		printf("  type: compact ascii\n");
 	else
 		printf("  type: compact unicode object\n");
-	printf("  length: %ld\n", len);
-	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &len));
+	printf("  length: %ld\n", lenght);
+	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &lenght));
 }
