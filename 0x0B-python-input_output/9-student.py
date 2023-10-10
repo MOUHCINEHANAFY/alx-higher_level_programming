a7290f98b4f7f5bf4c29972a9151dba5a473b139
MOUHCINEHANAFY <hanafymouhcine@gmail.com>
2023-10-10 19:57:33 +0000

    9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
-"""
-Class Student 
-"""
+"Create a class Student."""
 
 
 class Student:
-    def __init__(self, first_name, last_name, age):
-        """ initialize"""
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
+    """student."""
 
-    def to_json(self):
-        """ return a dict of self """
-        return vars(self)
+    def __init__(self, first_name, last_name, age):
+     """Initialize nv Student.
+
+     Args:
+         first_name (str): First name.
+         last_name (str): Last name.
+         age (int): Age
+         """
+         self.first_name = first_name
+         self.last_name = last_name
+         self.age = age
+         def to_json(self):
+         """Get a dictionary representation of the Student."""
+         return self.__dict__
