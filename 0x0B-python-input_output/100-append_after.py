a94778994bbc355fea0bdc84e71fb3fecaf25c42
MOUHCINEHANAFY <hanafymouhcine@gmail.com>
2023-10-10 20:07:01 +0000

    100

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,19 @@
+"""innsertion fct"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Insert text.
+
+    Args:
+        filename (str): File name.
+        search_string (str): Lookup string.
+        new_string (str): The string to add.
+        """
+        text = ""
+        with open(filename) as r:
+            for line in r:
+                text += line
+                if search_string in line:
+                    text += new_string
+        with open(filename, "w") as w:
+        w.write(text)
