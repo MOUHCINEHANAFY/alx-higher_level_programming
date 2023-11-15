2f307e5542b06291ffbdad6bd26d88e60757d1aa
MOUHCINEHANAFY <hanafymouhcine@gmail.com>
2023-11-15 19:18:59 +0000

    code readability

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,2 +1,5 @@
 --litst rows and show containt of db
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+SELECT ts.title, tsg.genre_id
+FROM tv_shows ts
+INNER JOIN tv_show_genres tsg ON ts.id = tsg.show_id
+ORDER BY ts.title ASC, tsg.genre_id ASC;
