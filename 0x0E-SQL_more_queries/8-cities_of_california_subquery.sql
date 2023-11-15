f445118334279279fa11bedb152875f6a60e847b
MOUHCINEHANAFY <hanafymouhcine@gmail.com>
2023-11-15 19:21:01 +0000

    correction

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,6 @@
 --lists of cities of CRA in the db ordred by id
-SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY id ASC;
+SELECT id, name FROM cities
+WHERE state_id = (
+	      SELECT id FROM states
+	      WHERE name = "California")
+ORDER BY id;
