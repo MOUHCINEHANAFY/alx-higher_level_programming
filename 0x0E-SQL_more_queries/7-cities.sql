d8602761f7524a5fd6416038fb5339ed22f70fc7
MOUHCINEHANAFY <hanafymouhcine@gmail.com>
2023-11-15 19:09:02 +0000

    correction

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,4 +1,10 @@
 -- Script that creates the table unique_id on your MySQL server
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (id INT AUTO_INCREMENT UNIQUE, state_id INT NOT NULL, name VARCHAR(256) NOT NULL, PRIMARY KEY (id), FOREIGN KEY (state_id) REFERENCE states(id));
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
+	PRIMARY KEY(`id`),
+	`id`       INT          NOT NULL AUTO_INCREMENT,
+	`state_id` INT          NOT NULL,
+	`name`     VARCHAR(256) NOT NULL,
+	FOREIGN KEY(`state_id`)
+	REFERENCES `hbtn_0d_usa`.`states`(`id`)
+);
