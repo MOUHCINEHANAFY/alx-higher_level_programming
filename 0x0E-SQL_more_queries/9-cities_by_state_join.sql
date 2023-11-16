528a12881effe601a594924346a78c011b7d2a2d
MOUHCINEHANAFY <hanafymouhcine@gmail.com>
2023-11-16 19:57:00 +0000

    task 9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,4 @@
---List cities
---list row of particular in db
-SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN states ON states.id = cities.state_id ORDER BY cities.id;
+-- Ce script répertorie toutes les villes contenues dans la base de données "hbtn_0d_usa"
+SELECT cities.id, cities.name, states.name
+FROM cities
+INNER JOIN states ON cities.state_id = states.id;
